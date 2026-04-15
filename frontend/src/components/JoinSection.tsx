c84808023f03224e4a1f81e1752ff4fd2b85a8f6
Aitor <aitorcyberlab@gmail.com>
2026-04-15 15:21:12 +0200

    hola

--- frontend/src/components/JoinSection.tsx
+++ frontend/src/components/JoinSection.tsx
@@ -23,7 +23,7 @@ export default function JoinSection() {
     setLoading(true);
 
     try {
-      const res = await fetch('https://aextreciber-backend.onrender.com', {
+      const res = await fetch('https://aextreciber-backend.onrender.com/send-email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
